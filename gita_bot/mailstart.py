d64c16eeda1c1001a08c4c0df09266a1e41c0f87
yashlab <yashlaboratoire@gmail.com>
2021-04-10 12:57:45 +0530

    gmail api based mailing

---
+++ gita_bot/mailstart.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import mimetypes
+from mailer_with_attach import *
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send',
+          'https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_id.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    
+    msg = create_message_with_attachment('yashlaboratoire@gmail.com','ygupta32@gmail.com','OAuth Based E-Mail','This is sent via app and VS!!','quote.png')
+    send_message(service,'me',msg)
+
+if __name__ == '__main__':
+    main()
+
+
+
